982353b38ab1683dc32f11f1f5c8b5b7c7ea7b17
VesaP1 <vp70277@ubt-uni.net>
2025-12-01 12:00:23 +0100

    Faza e pare - Perditesimet fillestare

---
+++ TimeWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeWeb.Models;
+
+namespace TimeWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+
+        { 
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id =1 , Name = "Luxury Watches" , DisplayOrder= 1 },
+                new Category { Id =2 , Name = "Women's Watches", DisplayOrder= 2 },
+                new Category { Id =3 , Name = "Men's Watches", DisplayOrder = 3 }
+                );
+        }
+    }
+}
