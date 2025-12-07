23155bc148e11c56905340566351bb641d80c9a7
Mirek2004 <mirekabashi8@gmail.com>
2025-12-07 19:23:49 +0100

    Update CategoryController.cs
    Fixed AddModelError property name used nameof(Index) for redirects and improved readabilit

--- TimeWeb/Controllers/CategoryController.cs
+++ TimeWeb/Controllers/CategoryController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using TimeWeb.Data;
 using TimeWeb.Models;
 
@@ -13,73 +13,64 @@ namespace TimeWeb.Controllers
             _db = db;
         }
 
-        
         public IActionResult Index()
         {
-            
-            List<Category> objCategoryList = _db.Categories.ToList();
-            return View(objCategoryList);
+            var categories = _db.Categories.ToList();
+            return View(categories);
         }
 
-        
         [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
 
-        
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            
             if (obj.Name == obj.DisplayOrder.ToString())
             {
-                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name.");
             }
 
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction(nameof(Index));
             }
 
             return View(obj);
         }
 
-        
+        [HttpGet]
         public IActionResult Edit(int? id)
         {
             if (id == null || id == 0)
-            {
                 return NotFound();
-            }
 
-            Category categoryFromDb = _db.Categories.Find(id);
-
-            if (categoryFromDb == null)
-            {
+            var category = _db.Categories.Find(id);
+            if (category == null)
                 return NotFound();
-            }
 
-            return View(categoryFromDb);
+            return View(category);
         }
 
-        
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Edit(Category obj)
         {
             if (obj.Name == obj.DisplayOrder.ToString())
             {
-                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name.");
             }
 
             if (ModelState.IsValid)
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction(nameof(Index));
             }
 
             return View(obj);
